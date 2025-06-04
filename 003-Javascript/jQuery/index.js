c8b9e5631ffe98308c5c9002211704d75483b4d1
Rahman2782 <rahman2782@gmail.com>
2025-06-04 03:15:12 -0400

    more jQuery

--- 003-Javascript/jQuery/index.js
+++ 003-Javascript/jQuery/index.js
@@ -1,2 +1,25 @@
 $("h1").css("color", "red");
+$("button").css("color", "green");
+$("h1").text("Bye");
+$("button").html("<em>Test</em>").css("color", "blue");
+// $("img").attr("src");
+$("a").attr("href", "https://www.yahoo.com");
 
+$("h1").click(function() {
+    $("h1").css("color", "purple");
+});
+
+$("button").click(function() {
+    $("h1").css("color", "yellow");
+});
+
+$("input").keypress(function(event) { //putting body instead of input will detect keys everywhere
+    $("h1").text(event.key);
+});
+
+$("h1").click(function() {
+    $("h1").css("color", "green");
+    setTimeout(function() {
+        $("h1").css("color", "purple");
+    }, 110);
+});
