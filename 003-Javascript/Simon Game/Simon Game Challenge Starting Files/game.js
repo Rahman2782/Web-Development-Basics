ed392adf1f82570214c0659b72b38a5f0e7344c5
Rahman2782 <rahman2782@gmail.com>
2025-06-11 02:15:42 -0400

    added comments

--- 003-Javascript/Simon Game/Simon Game Challenge Starting Files/game.js
+++ 003-Javascript/Simon Game/Simon Game Challenge Starting Files/game.js
@@ -4,6 +4,9 @@ var userClickedPattern = [];
 var level = 0;
 var start = 0;
 
+//when any button is clicked, its colorID is added to the back of an array
+//and a sound+animation is played. Then the last index of the players array is
+//sent to a function to check if they are correct.
 $(".btn").click(function() {
     var userChosenColour = $(this).attr('id');
     userClickedPattern.push(userChosenColour);
@@ -14,6 +17,7 @@ $(".btn").click(function() {
     checkAnswer(last);
 })
 
+//adds the pressed css class and removes it after a 100ms timeout
 function animatePress(currentColour) {
     $(`#${currentColour}`).addClass("pressed");
 
@@ -22,9 +26,13 @@ function animatePress(currentColour) {
     }, 100);
 }
 
+//resets the user's array and randomly selects a colour, then adds the colour
+//to the end of the game array. Plays a brief animation to show the next button 
+//to be pressed and updates the h1 to match the number of buttons to be 
+//pressed in the coming sequence
 function nextSequence() {
     userClickedPattern = [];
-    var randomNumber = Math.floor(Math.random() * 4);
+    var randomNumber = Math.floor(Math.random() * 4); //floor rounds to an int instead of giving a float
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
@@ -32,9 +40,9 @@ function nextSequence() {
     playSound(randomChosenColour);
     $(`#level-title`).text(`Level ${level}`);
     level++;
-
 }
 
+//plays a sound from the sounds folder, throws an error if it cannot be played
 function playSound(name) {
     var audio = new Audio(`sounds/${name}.mp3`);
     audio.play().catch(function(error) {
@@ -42,8 +50,9 @@ function playSound(name) {
     });
 }
 
+//if the ends of the 2 arrays match, then the game is continued until the entire
+//sequence matches. Then the next sequence is generated after a brief timeout.
 function checkAnswer(currentLevel) {
-    console.log(currentLevel);
     if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
         console.log("success");
         if(userClickedPattern.length === gamePattern.length) {
@@ -51,7 +60,7 @@ function checkAnswer(currentLevel) {
                 nextSequence();
             }, 1000);
         }
-    }
+    } //if wrongm the screen flashes and resets the game state
     else {
         playSound("wrong");
         $("body").addClass("game-over");
@@ -70,6 +79,7 @@ function startOver() {
     userClickedPattern = [];
 }
 
+//starts the game if a key is pressed (only called once to start the game)
 $(document).keypress(function () {
     start++;
     if(start === 1) {
