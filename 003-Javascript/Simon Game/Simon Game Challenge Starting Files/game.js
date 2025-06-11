7d929c812a648c53b920746a6c18d5002b89da32
Rahman2782 <rahman2782@gmail.com>
2025-06-11 01:54:15 -0400

    added game over logic

--- 003-Javascript/Simon Game/Simon Game Challenge Starting Files/game.js
+++ 003-Javascript/Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var start = 0;
 
 $(".btn").click(function() {
     var userChosenColour = $(this).attr('id');
@@ -8,6 +10,8 @@ $(".btn").click(function() {
     console.log(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    var last = userClickedPattern.length - 1;
+    checkAnswer(last);
 })
 
 function animatePress(currentColour) {
@@ -19,12 +23,16 @@ function animatePress(currentColour) {
 }
 
 function nextSequence() {
+    userClickedPattern = [];
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+    $(`#level-title`).text(`Level ${level}`);
+    level++;
+
 }
 
 function playSound(name) {
@@ -34,4 +42,32 @@ function playSound(name) {
     });
 }
 
-nextSequence();
+function checkAnswer(currentLevel) {
+    console.log(currentLevel);
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $(`#level-title`).text("Game Over, Press Any Key to Restart");
+        start = 0;
+        gamePattern = [];
+        userClickedPattern = [];
+    }
+} 
+
+$(document).keypress(function () {
+    start++;
+    if(start === 1) {
+        nextSequence();
+    }
+})
